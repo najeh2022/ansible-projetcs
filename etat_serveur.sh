8f327101bb1ac572d3615effba97c5bf91ddfcd7
najeh <najehhattab66@gmail.com>
2026-05-02 03:05:18 +0200

    test8

--- etat_serveur.sh
+++ etat_serveur.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
-host_name=$host_name
+host_name=$(hostname)
+date=$(date)
 addresse_ip_prive=$(nmcli connection show Livebox-6EE0 | grep "IP4.ADDRESS" | cut -d ":" -f2)
 addresse_ip=$(curl -4 ifconfig.me)
 USED_CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | cut -d'%' -f1)"%"
 USED_RAM=$(free -h | grep Mem | awk '{print $3}')
 USED_DISK=$(df -h | grep sda2 | cut -d " " -f20)
 
-echo "les info de serveur pour $date"
+echo "les info de serveur pour la date $date"
 
 echo "Host Name:" $host_name
 
